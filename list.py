5335dcb94840d0e19433dee9cb35f04795b2eebd
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-07 23:18:02 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,58 +1,58 @@
 marks = [3,5,6,"mili",True , 31 , 34 , 56 , 78, 45 , 342 ]
-# print(marks)
-# print(type(marks))
-# print(marks[0]) #indexing
-# print(marks[1]) #indexing 
-# print(marks[2]) #indexing
-# print(marks[3]) #indexing
-# print(marks[4]) #indexing
+print(marks)
+print(type(marks))
+print(marks[0]) #indexing
+print(marks[1]) #indexing 
+print(marks[2]) #indexing
+print(marks[3]) #indexing
+print(marks[4]) #indexing
 
 
-# print(marks[-3]) #negative index
-# print(marks[len(marks)-3]) #positive index
-# print(marks[5-3]) #positive index
-# print(marks[2]) #positive index
+print(marks[-3]) #negative index
+print(marks[len(marks)-3]) #positive index
+print(marks[5-3]) #positive index
+print(marks[2]) #positive index
 
-# if 7 in marks:
-#     print("Yes")
-# else:
-#     print("No")
-
-
-# if "mili" in marks:
-#     print("Yes")
-# else:
-#     print("No")
+if 7 in marks:
+    print("Yes")
+else:
+    print("No")
 
 
-# if "6" in marks:
-#     print("Yes")
-# else:
-#     print("No")
+if "mili" in marks:
+    print("Yes")
+else:
+    print("No")
 
 
-# if "li" in "mili": #same thing is applied for string as well
-#     print("YES")
-# else:
-#     print("NO")
+if "6" in marks:
+    print("Yes")
+else:
+    print("No")
 
 
-# print(marks)
-# print(marks[1:])
-# print(marks[:3])
-# print(marks[-3:])
-# print(marks[:-3])
-# print(marks[1:-3])
-# print(marks[1:4:2])
-# print(marks[1:2:8])
-# print(marks[4:7:10])
-# print(marks[3:6:7])
+if "li" in "mili": #same thing is applied for string as well
+    print("YES")
+else:
+    print("NO")
 
 
-# lst = [i*i for i in range (10)]
-# print(lst)
-# lst = [i*i for i in range (10) if i%2==0]
-# print(lst)
+print(marks)
+print(marks[1:])
+print(marks[:3])
+print(marks[-3:])
+print(marks[:-3])
+print(marks[1:-3])
+print(marks[1:4:2])
+print(marks[1:2:8])
+print(marks[4:7:10])
+print(marks[3:6:7])
+
+
+lst = [i*i for i in range (10)]
+print(lst)
+lst = [i*i for i in range (10) if i%2==0]
+print(lst)
 
 
 marks = [33,45,67]
