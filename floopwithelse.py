ede2552941bc63990956882ab7f972ab403386e9
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-10 17:30:04 +0530

    Add files via upload

---
+++ floopwithelse.py
@@ -0,0 +1,50 @@
+# for i in range(5):
+#     print(i)
+
+# else:
+#     print("Sorry")
+
+
+
+# for i in []:
+#     print(i)
+
+# else:
+#     print("Sorry")
+
+
+
+# for i in range (6):
+#     print(i)
+
+#     if i == 4:
+#         break
+
+# else:
+#     print("Sorry")
+
+
+
+# i=0
+# while i<7:
+#     print(i)
+#     i=i+1
+#     if i == 4:
+#         break
+
+# else:
+#     print("Sorry")
+
+
+
+for x in range(5):
+    print("Iteratio no {} in for loop ". format(x+1))
+else:
+    print("Else block in loop")
+    print("Out of the loop")
+
+
+
+
+
+
