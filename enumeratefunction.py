5369177868f967ef9fb0d343150d2380a72536e0
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-15 00:24:03 +0530

    Add files via upload

---
+++ enumeratefunction.py
@@ -0,0 +1,34 @@
+marks = [12 , 56 , 32 , 98 , 54 , 23 , 54 , 5 , 78]
+
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if (index == 3):
+#         print("Harry , Awesome!")
+#     index+=1
+
+for index, mark in enumerate(marks , start=1):
+    print(mark)
+    if (index == 3):
+        print("Harry , Awesome!")
+    # index+=1
+
+# print(marks.index(98))
+# print("Harry , awesome")
+
+# nancy = ["Tuihi" , 19 , "George" , "Thangiu"]
+
+# for index , name in enumerate(nancy , start=1):
+#     print(name)
+#     if (index == 0):
+#         print("Name:")
+#     elif (index == 1):
+#         print("Age:")
+#     elif(index == 2):
+#         print("Surname:")
+#     elif(index == 3):
+#         print("Father's Name:")
+        
+
+
