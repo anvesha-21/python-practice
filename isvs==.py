a06a9b8e928f07ebdf989b4ff8b557f44cfd339d
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-07-08 21:02:14 +0530

    Add files via upload

---
+++ isvs==.py
@@ -0,0 +1,21 @@
+# a = [1,2,43]
+# b = [1,2,43]
+
+# a = 3
+# b = 3
+
+# a="kerry"
+# b="kerry"
+
+# a=(1,2)
+# b=(1,2)
+
+# print(a is b) #Exact location of object in memory
+# print(a == b) #value
+
+a= None
+b= None
+
+print(a is b)
+print(a is None)
+print(a == b)
