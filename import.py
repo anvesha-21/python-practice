55b4406643f095103dfa8415c95215273118516a
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2026-05-12 09:51:07 +0530

    import.py
    Implement various mathematical operations and imports.

---
+++ import.py
@@ -0,0 +1,27 @@
+import math
+
+result =  math.sqrt(9)
+print (result)
+
+from math import  sqrt , pi
+
+result = sqrt(9) * pi
+print(result)
+
+# as Function
+
+from math import  sqrt as s , pi
+
+result = s(9) * pi
+print(result)
+
+# dir
+
+print(dir(math))
+print(math.ulp , type (math.ulp))
+
+# import module
+
+from cici import * 
+welcome()
+print(cici)
